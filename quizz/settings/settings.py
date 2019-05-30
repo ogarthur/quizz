b113a540eb53c18fa3d58841d2fe61b4d30a9af6
Arturo <olmos.arthur@gmail.com>
2019-05-31 00:39:51 +0200

    fixed settings base_dir

--- quizz/settings/settings.py
+++ quizz/settings/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 TEMPLATE_DIR = os.path.join(BASE_DIR, '../../templates')
 STATIC_DIR = os.path.join(BASE_DIR, '../../static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
